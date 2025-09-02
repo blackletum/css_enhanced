72ffe7c2e87deff60fecf8b6a130d1aa2946a112
kamay <admin@xutaxkamay.com>
2025-09-02 17:50:40 +0200

    Ahead clock adjustement causes issues still with interp

--- engine/servermsghandler.cpp
+++ engine/servermsghandler.cpp
@@ -848,12 +848,12 @@ bool CClientState::ProcessPacketEntities( SVC_PacketEntities *msg )
 			}
 			// FIXME: We really need a better adjustement ...
 			// // If we drifted too much from interpolation amount, we might increase dynamically the window size ?
-			else if ( g_ClientGlobalVariables.predicted_snapshot_tickcount
-					  >= m_nSnapshotTickCount + nMaximumInterpolationAmountInTicks )
-			{
-				// Give it at least a tick
-				g_ClientGlobalVariables.predicted_snapshot_tickcount = m_nSnapshotTickCount;
-			}
+			// else if ( g_ClientGlobalVariables.predicted_snapshot_tickcount
+			// 		  >= m_nSnapshotTickCount + nMaximumInterpolationAmountInTicks )
+			// {
+			// 	// Give it at least a tick
+			// 	g_ClientGlobalVariables.predicted_snapshot_tickcount = m_nSnapshotTickCount;
+			// }
 
 			// Preprocessing primarily does client prediction. So if we're processing deltas--do it
 			// otherwise, we're about to be told exactly what the state of everything is--so skip it.
